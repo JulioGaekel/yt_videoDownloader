33dafc22e4267f46d94a8336d1791e8ace3fca52
Julio Gaekel <juliogaekel@gmail.com>
2024-06-11 14:23:12 +0200

    Add TODOs
    #TODO: Add error handling (try-except) and print any errors that occur during the download process
    #TODO: Add feature optional path, if nothing is declared, have default path.
    #TODO: Validate that the path exists, if it does exist, it should create it.
    #TODO: Organize code. Devide into functions and add main function.
    #TODO: Add comments and refactor for better readability.

--- ytDownloader.py
+++ ytDownloader.py
@@ -12,3 +12,9 @@ print('Views: ', yt.views)
 yd = yt.streams.get_highest_resolution()
 
 yd.download('/Users/juliogaekel/Documents/youtubeVideos')
+
+#TODO: Add error handling (try-except) and print any errors that occur during the download process
+#TODO: Add feature optional path, if nothing is declared, have default path.
+#TODO: Validate that the path exists, if it does exist, it should create it.
+#TODO: Organize code. Devide into functions and add main function.
+#TODO: Add comments and refactor for better readability.
